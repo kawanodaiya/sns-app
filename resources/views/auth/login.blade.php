2d2e93576fedd6c2ce7524af58ff253912859d00
kawanodaiya <c612001234@tokushima-u.ac.jp>
2022-11-19 10:53:02 +0900

    ten commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,31 +16,20 @@
                     <div class="card-text">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
-
                             <div class="md-form">
                                 <label for="email">メールアドレス</label>
                                 <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
                             </div>
-
                             <div class="md-form">
                                 <label for="password">パスワード</label>
                                 <input class="form-control" type="password" id="password" name="password" required>
                             </div>
-
                             <input type="hidden" name="remember" id="remember" value="on">
-
-                            <div class="text-left">
-                                <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
-                            </div>
-
                             <button class="btn btn-block bg-success text-white mt-2 mb-2" type="submit">ログイン</button>
-
                         </form>
-
                         <div class="mt-0">
                             <a href="{{ route('register') }}" class="card-text">ユーザー登録はこちら</a>
                         </div>
-
                     </div>
                 </div>
             </div>
